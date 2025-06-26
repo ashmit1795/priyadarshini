77896f746c8061eedd30e58d6f8c85d8dde6da74
ashmit1795 <ashmitpatra77@gmail.com>
2025-06-27 02:38:33 +0530

    feat: add Clerk themes integration for enhanced appearance in application

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import config from "./config/config.js";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { dark } from "@clerk/themes";
 
 const PUBLISHABLE_KEY = config.clerkPublishableKey;
 
@@ -12,7 +13,7 @@ if (!PUBLISHABLE_KEY) {
 }
 
 createRoot(document.getElementById("root")).render(
-	<ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+	<ClerkProvider appearance={{ baseTheme: dark }} publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
 		<BrowserRouter>
 			<App />
 		</BrowserRouter>
