a89268565d84221280a42fefe7737c4e3409f86c
ashmit1795 <ashmitpatra77@gmail.com>
2025-06-27 21:13:33 +0530

    feat: add DateSelect and Loading components for date selection and loading indication

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,9 +1,11 @@
 import BlurCircle from "./BlurCircle/BlurCircle.jsx";
+import DateSelect from "./DateSelect/DateSelect.jsx";
 import FeaturedSection from "./FeaturedSection/FeaturedSection.jsx";
 import Footer from "./Footer/Footer.jsx";
 import HeroSection from "./HeroSection/HeroSection.jsx";
+import Loading from "./Loading/Loading.jsx";
 import MovieCard from "./MovieCard/MovieCard.jsx";
 import Navbar from "./Navbar/Navbar.jsx";
 import TrailersSection from "./TrailersSection/TrailersSection.jsx";
 
-export { Footer, Navbar, HeroSection, FeaturedSection, BlurCircle, MovieCard, TrailersSection };
+export { Footer, Navbar, HeroSection, FeaturedSection, BlurCircle, MovieCard, TrailersSection, DateSelect, Loading };
