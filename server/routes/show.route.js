b77ec06134e53817f2aba0acd66325b4b49bfbb8
ashmit1795 <ashmitpatra77@gmail.com>
2025-07-04 18:04:46 +0530

    feat: add protectAdmin middleware to secure show routes

--- server/routes/show.route.js
+++ server/routes/show.route.js
@@ -1,9 +1,10 @@
 import express, { Router } from "express";
 import { addShows, getNowPlayingMovies } from "../controllers/show.controller.js";
+import protectAdmin from "../middlewares/auth.middleware.js";
 
 const showRouter = Router();
 
-showRouter.get("/now-playing", getNowPlayingMovies);
-showRouter.post("/add", addShows);
+showRouter.get("/now-playing", protectAdmin, getNowPlayingMovies);
+showRouter.post("/add", protectAdmin, addShows);
 
 export default showRouter;
