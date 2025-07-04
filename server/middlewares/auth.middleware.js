1b8e1ac5f23b11fd31bf631817b4e66d6e2c5522
ashmit1795 <ashmitpatra77@gmail.com>
2025-07-05 00:23:07 +0530

    fix: update response status in protectAdmin middleware for consistency

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -6,7 +6,7 @@ const protectAdmin = async (req, res, next) => {
         const { userId } = req.auth();
         const user = await clerkClient.users.getUser(userId);
         if (user.privateMetadata.role !== "admin") {
-            return res.status(403).json({ success: false, message: "Forbidden" });
+            return res.json({ success: false, message: "Forbidden" });
         }
         next();
     } catch (error) {
