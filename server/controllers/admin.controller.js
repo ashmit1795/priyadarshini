5ab6842e6108f561a4615100044d50571f5f6c22
ashmit1795 <ashmitpatra77@gmail.com>
2025-07-04 19:43:25 +0530

    feat: add admin controller with endpoints for dashboard data, shows, and bookings

---
+++ server/controllers/admin.controller.js
@@ -0,0 +1,59 @@
+import Booking from "../models/booking.model.js";
+import Show from "../models/show.model.js";
+import User from "../models/user.model.js";
+
+// API endpoint to check if a user is an admin
+const isAdmin = async (req, res) => { 
+    res.json({ success: true, isAdmin: true });
+}
+
+// API endpoint to get dashboard data
+const getDashboardData = async (req, res) => {
+    try {
+        const bookings = await Booking.find({ isPaid: true });
+        const activeShows = await Show.find({ showDateTime: { $gte: new Date() } }).populate("movie");
+        const totalUsers = await User.countDocuments();
+
+        const dashboardData = {
+            totalBookings: bookings.length,
+            totalRevenue: bookings.reduce((total, booking) => total + booking.amount, 0),
+            activeShows,
+            totalUsers
+        };
+
+        res.json({ success: true, dashboardData });
+    } catch (error) {
+        console.error("Error getting dashboard data:", error.message);
+        res.status(500).json({ success: false, error: error.message });
+    }
+}
+
+// API endpoint to get all the shows
+const getAllShows = async (req, res) => {
+    try {
+        const shows = await Show.find({ showDateTime: { $gte: new Date() } }).populate("movie").sort({ showDateTime: 1 });
+        res.json({ success: true, shows });
+    } catch (error) {
+        console.error("Error getting all shows:", error.message);
+        res.status(500).json({ success: false, error: error.message });
+    }
+};
+
+// API endpoint to get all bookings
+const getAllBookings = async (req, res) => {
+    try {
+        const bookings = await Booking.find().populate("user").populate({
+            path: "show",
+            populate: {
+                path: "movie"
+            }
+        }).sort({ createdAt: -1 });
+
+        res.json({ success: true, bookings });
+    } catch (error) {
+        console.error("Error getting all bookings:", error.message);
+        res.status(500).json({ success: false, error: error.message });
+    }
+};
+
+export { isAdmin, getDashboardData, getAllShows, getAllBookings };
