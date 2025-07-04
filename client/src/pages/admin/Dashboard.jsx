ea8722fc98100907d9e1cb471677424dd4eef2c1
ashmit1795 <ashmitpatra77@gmail.com>
2025-07-05 02:06:58 +0530

    feat: implement dashboard data fetching and enhance active shows display

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react";
 import config from "../../config/config.js";
 import { ChartLineIcon, CircleDollarSignIcon, PlayCircleIcon, StarIcon, UserIcon } from "lucide-react";
-import { dummyDashboardData } from "../../assets/assets.js";
 import { BlurCircle, Loading, Title } from "../../components";
 import dateFormat from "../../lib/dateFormat.js";
+import toast from "react-hot-toast";
+import useAppContext from "../../hooks/useAppContext.js";
+import { useCallback } from "react";
 
 function Dashboard() {
-    const currency = config.currency;
+	const currency = config.currency;
+	
+	const { axios, getToken, user, imageBaseUrl } = useAppContext();
 
     const [dashboardData, setDashboardData] = useState({
         totalBookings: 0,
@@ -40,14 +44,28 @@ function Dashboard() {
 		},
 	];
     
-    const fetchDashboardData = () => {
-        setDashboardData(dummyDashboardData);
-        setLoading(false);
-    }
+    const fetchDashboardData = useCallback(async () => {
+		try {
+			const { data } = await axios.get("/admin/dashboard", {
+				headers: {
+					Authorization: `Bearer ${await getToken()}`,
+				},
+			});
+			if (data.success) {
+				setDashboardData(data.dashboardData);
+				setLoading(false);
+			} else {
+				toast.error(data.message || "Failed to fetch dashboard data");
+			}
+		} catch (error) {
+			console.error("Error fetching dashboard data:", error);
+			toast.error(error.message || "Failed to fetch dashboard data");
+		}
+	}, [axios, getToken]);
 
     useEffect(() => {
-        fetchDashboardData();
-    }, []);
+        if (user) fetchDashboardData();
+    }, [fetchDashboardData, user]);
 
     return !loading ? (
 		<>
@@ -71,28 +89,30 @@ function Dashboard() {
 				</div>
 
 				{/* Active Shows */}
-				<p className="mt-10 text-lg font-medium">Active Shows</p>
-				<div className="relative flex flex-wrap gap-6 mt-4 max-w-5xl">
-					<BlurCircle top="100px" left="-10%" />
-					{dashboardData.activeShows.map((show) => (
-						<div
-							key={show._id}
-							className="w-55 rounded-lg overflow-hidden pb-3 bg-primary/10 border border-primary/20 hover:-translate-y-1 transition duration-300"
-						>
-							<img src={show.movie.poster_path} alt="movie poster" className="h-60 w-full object-cover" />
-							<p className="font-medium p-2 truncate">{show.movie.title}</p>
-							<div className="flex items-center justify-between px-2">
-								<p className="text-lg font-medium">
-									{currency} {show.showPrice}
-								</p>
-								<p className="flex items-center gap-1 text-sm text-gray-400 mt-1 pr-1">
-									<StarIcon className="w-4 h-4 text-primary fill-primary" />
-									{show.movie.vote_average.toFixed(1)}
-								</p>
+				<div>
+					<p className="mt-10 text-lg font-medium">Active Shows</p>
+					<div className="relative flex flex-wrap gap-6 mt-4 max-w-5xl">
+						<BlurCircle top="100px" left="-10%" />
+						{dashboardData.activeShows.map((show) => (
+							<div
+								key={show._id}
+								className="w-55 h-full rounded-lg overflow-hidden pb-3 bg-primary/10 border border-primary/20 hover:-translate-y-1 transition duration-300"
+							>
+								<img src={`${imageBaseUrl}${show.movie.poster_path}`} alt="movie poster" className="h-60 w-full object-cover" />
+								<p className="font-medium p-2 truncate">{show.movie.title}</p>
+								<div className="flex items-center justify-between px-2">
+									<p className="text-lg font-medium">
+										{currency} {show.showPrice}
+									</p>
+									<p className="flex items-center gap-1 text-sm text-gray-400 mt-1 pr-1">
+										<StarIcon className="w-4 h-4 text-primary fill-primary" />
+										{show.movie.vote_average.toFixed(1)}
+									</p>
+								</div>
+								<p className="px-2 pt-2 text-sm text-gray-500">{dateFormat(show.showDateTime)}</p>
 							</div>
-							<p className="px-2 pt-2 text-sm text-gray-500">{dateFormat(show.showDateTime)}</p>
-						</div>
-					))}
+						))}
+					</div>
 				</div>
 			</div>
 		</>
