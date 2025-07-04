290edfebf37e73d3e0ed9f5bbacfac291bc1df4e
ashmit1795 <ashmitpatra77@gmail.com>
2025-07-04 22:42:47 +0530

    add: introduce a new `serverUrl` property with a default fallback to enhance configurability.

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,6 +1,7 @@
 const config = {
 	clerkPublishableKey: String(import.meta.env.VITE_CLERK_PUBLISHABLE_KEY),
-	currency: String(import.meta.env.VITE_CURRENCY)
+	currency: String(import.meta.env.VITE_CURRENCY),
+	serverUrl: String(import.meta.env.VITE_SERVER_URL) || "http://localhost:3000",
 };
 
 export default config;
