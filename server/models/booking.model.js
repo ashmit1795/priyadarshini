4501fc1330e13e5f500a14a291bde5e314392603
ashmit1795 <ashmitpatra77@gmail.com>
2025-07-04 19:22:41 +0530

    feat: add Booking model to handle user bookings and payment information

---
+++ server/models/booking.model.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const bookingSchema = new mongoose.Schema({
+	show: { type: String, ref: "Show", required: true },
+    user: { type: String, ref: "User", required: true },
+    amount: { type: Number, required: true },
+    bookedSeats: { type: Array, required: true },
+    isPaid: { type: Boolean, default: false },
+    paymentLink: { type: String, default: null },
+}, {timestamps: true});
+
+const Booking = mongoose.model("Booking", bookingSchema);
+
+export default Booking;
