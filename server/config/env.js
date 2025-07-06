d18d659c62f9e1a01e8a740d167985c1de9579a0
ashmit1795 <ashmitpatra77@gmail.com>
2025-07-06 18:29:18 +0530

    feat: add Cashfree API credentials to environment variables

--- server/config/env.js
+++ server/config/env.js
@@ -4,4 +4,4 @@ config({
     path: `.env`
 })
 
-export const { MONGODB_URL, PORT, TMDB_API_KEY } = process.env;
+export const { MONGODB_URL, PORT, TMDB_API_KEY, CASHFREE_APP_ID, CASHFREE_SECRET_KEY, SERVER_URL } = process.env;
