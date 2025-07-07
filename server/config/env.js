61a989ffb71e83a9308903c116c52a73dd32b4a5
ashmit1795 <ashmitpatra77@gmail.com>
2025-07-08 01:07:36 +0530

    feat: add email configuration variables to environment settings

--- server/config/env.js
+++ server/config/env.js
@@ -4,4 +4,14 @@ config({
     path: `.env`
 })
 
-export const { MONGODB_URL, PORT, TMDB_API_KEY, CASHFREE_APP_ID, CASHFREE_SECRET_KEY, SERVER_URL } = process.env;
+export const {
+    MONGODB_URL,
+    PORT,
+    TMDB_API_KEY,
+    CASHFREE_APP_ID,
+    CASHFREE_SECRET_KEY,
+    SERVER_URL,
+    SENDER_EMAIL,
+    SMTP_USER,
+    SMTP_PASS
+} = process.env;
