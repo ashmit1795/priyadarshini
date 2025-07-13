a3488e403243ed75d30fe8d61816258bb2eda2aa
ashmit1795 <ashmitpatra77@gmail.com>
2025-07-14 00:25:20 +0530

    fix: conditionally render "View All" button in FeaturedSection component

--- client/src/components/FeaturedSection/FeaturedSection.jsx
+++ client/src/components/FeaturedSection/FeaturedSection.jsx
@@ -9,21 +9,23 @@ function FeaturedSection() {
 
 	const { shows } = useAppContext();
 	
-	return (
+	return shows.length > 0 ? (
 		<div className="px-6 md:px-16 lg:px-24 cl:px-44 overflow-hidden">
 			<div className="relative flex items-center justify-between pt-20 pb-10">
 				<BlurCircle top="0" right="-80px" />
 				<p className="text-gray-300 font-medium text-lg">Now Showing</p>
-				<button
-					onClick={() => {
-						navigate("/movies");
-						scrollTo(0, 0);
-					}}
-					className="group flex items-center gap-2 text-sm text-gray-300 cursor-pointer"
-				>
-					View All
-					<ArrowRight className="group-hover:translate-x-0.5 transition w-4.5 h-4.5" />
-				</button>
+				{ shows.length > 4 && (
+					<button
+						onClick={() => {
+							navigate("/movies");
+							scrollTo(0, 0);
+						}}
+						className="group flex items-center gap-2 text-sm text-gray-300 cursor-pointer"
+					>
+						View All
+						<ArrowRight className="group-hover:translate-x-0.5 transition w-4.5 h-4.5" />
+					</button>
+				)}
 			</div>
 			<div className="flex flex-wrap max-sm:justify-center gap-8 mt-8">
 				{shows.slice(0, 4).map((show) => (
@@ -44,6 +46,8 @@ function FeaturedSection() {
 				</div>
 			)}
 		</div>
+	) : (
+		<div className="text-center text-gray-500 py-20">No movies are currently available.</div>
 	);
 }
 
