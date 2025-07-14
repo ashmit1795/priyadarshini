0df944d54e26bc4fed190d84c28df2c2996daaef
ashmit1795 <ashmitpatra77@gmail.com>
2025-07-14 21:34:04 +0530

    feat: enhance AdminSidebar to fetch and display user profile dynamically

--- client/src/components/admin/AdminSidebar/AdminSidebar.jsx
+++ client/src/components/admin/AdminSidebar/AdminSidebar.jsx
@@ -1,13 +1,42 @@
-import { LayoutDashboardIcon, ListCollapseIcon, ListIcon, PlusSquareIcon, ScanQrCodeIcon } from "lucide-react";
-import { assets } from "../../../assets/assets.js";
+import { LayoutDashboardIcon, ListCollapseIcon, ListIcon, LoaderCircle, PlusSquareIcon, ScanQrCodeIcon } from "lucide-react";
 import { NavLink } from "react-router-dom";
+import useAppContext from "../../../hooks/useAppContext.js";
+import { useState, useEffect } from "react";
+import { useClerk } from "@clerk/clerk-react";
+
 
 function AdminSidebar() {
-    const user = {
-        firstName: "Admin",
-        lastName: "User",
-        imageUrl: assets.profile
-    }
+
+	const { openUserProfile } = useClerk();
+	
+	
+	const [user, setUser] = useState({
+		name: "Admin User",
+		image: ""
+	});
+
+	const { axios, getToken } = useAppContext();
+
+	const fetchUser = async () => {
+		try {
+			const { data } = await axios.get("/user/", {
+				headers: {
+					Authorization: `Bearer ${await getToken()}`,
+				}
+			});
+			if (data.success) {
+				setUser(data.user);
+			}
+		} catch (error) {
+			console.error("Error fetching user:", error);
+			
+		}
+	}
+
+	useEffect(() => {
+		fetchUser();
+	}, []);
+
 
     const adminNavLinks = [
         { name: "Dashboard", path: "/admin", icon: LayoutDashboardIcon },
@@ -20,16 +49,18 @@ function AdminSidebar() {
 
     return (
 		<div className="h-[calc(100vh-64px)] md:flex flex-col items-center pt-8 max-w-13 md:max-w-60 w-full border-r border-gray-300/20 text-sm">
-			<img className="h-9 md:h-14 w-9 md:w-14 rounded-full mx-auto" src={user.imageUrl} alt="profile picture" />
-			<p className="mt-2 text-base max-md:hidden">
-				{user.firstName} {user.lastName}
-			</p>
+			{user.image ? (
+				<img onClick={openUserProfile} className="h-9 md:h-14 w-9 md:w-14 rounded-full mx-auto" src={user.image} alt="profile picture" />
+			) : (
+				<LoaderCircle className="animate-spin w-4 h-4" />
+			)}
+			<p className="mt-2 text-base max-md:hidden">{user.name}</p>
 			<div className="w-full">
 				{adminNavLinks.map((link, idx) => (
 					<NavLink
-                        key={idx}
-                        to={link.path}
-                        end
+						key={idx}
+						to={link.path}
+						end
 						className={({ isActive }) =>
 							`relative flex items-center max-md:justify-center gap-2 w-full py-2.5 min-md:pl-10 first:mt-6 text-gray-400 ${
 								isActive && "bg-primary/15 text-primary group"
