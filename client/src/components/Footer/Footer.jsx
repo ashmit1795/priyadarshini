33ee070d288091ab2ac3b2d0940483eb6b6fd0d9
ashmit1795 <ashmitpatra77@gmail.com>
2025-07-15 23:18:26 +0530

    feat: update Footer component with dynamic links and improved content

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom";
 
 function Footer() {
-  return (
+	const footerLinks = [
+		{ title: "Home", url: "/" },
+		{ title: "About us", url: "/about-us" },
+		{ title: "Terms and Conditions", url: "/terms-and-conditions" },
+		{ title: "Contact us", url: "/contact-us" },
+		{ title: "Privacy policy", url: "/privacy-policy" },
+	];
+
+	return (
 		<footer className="px-6 mt-40 md:px-16 lg:px-36 w-full text-gray-300">
 			<div className="flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-500 pb-14">
 				<div className="md:max-w-96">
 					<img className="w-72 h-auto" src={assets.logo} alt="logo" />
 					<p className="mt-6 text-sm">
-						Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-						and scrambled it to make a type specimen book.
+						Priyadarshini is more than just a movie platform — it’s a celebration of storytelling, culture, and connection. From timeless
+						classics to the latest blockbusters, we bring cinema to life with comfort, quality, and a passion for unforgettable
+						experiences.
 					</p>
 					<div className="flex items-center gap-2 mt-4">
 						<img src={assets.googlePlay} alt="google play" className="h-9 w-auto" />
@@ -20,32 +30,27 @@ function Footer() {
 					<div>
 						<h2 className="font-semibold mb-5">Company</h2>
 						<ul className="text-sm space-y-2">
-							<li>
-								<a href="/">Home</a>
-							</li>
-							<li>
-								<a href="#">About us</a>
-							</li>
-							<li>
-								<a href="#">Contact us</a>
-							</li>
-							<li>
-								<a href="#">Privacy policy</a>
-							</li>
+							{footerLinks.map((link, index) => (
+								<li key={index}>
+									<Link to={link.url} className="hover:text-primary transition">
+										{link.title}
+									</Link>
+								</li>
+							))}
 						</ul>
 					</div>
 					<div>
 						<h2 className="font-semibold mb-5">Get in touch</h2>
 						<div className="text-sm space-y-2">
-							<p>+91-234-567-890</p>
-							<p>contact@priyadarshini.com</p>
+							<p>+91-8480476322</p>
+							<p>ashmitpatra77@gmail.com</p>
 						</div>
 					</div>
 				</div>
 			</div>
-			<p className="pt-4 text-center text-sm pb-5">Copyright {new Date().getFullYear()} © Priyadarshini. All Right Reserved.</p>
+			<p className="pt-4 text-center text-sm pb-5">Copyright {new Date().getFullYear()} © Priyadarshini Cinemas. All Right Reserved.</p>
 		</footer>
-  );
+	);
 }
 
 export default Footer;
