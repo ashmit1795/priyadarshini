5a9ed11f8c0831f6bce28f3bac7fae2605d99e27
ashmit1795 <ashmitpatra77@gmail.com>
2025-07-16 01:52:03 +0530

    feat: implement checkDBConnection utility for database connection validation

---
+++ server/utils/checkDBConnection.js
@@ -0,0 +1,9 @@
+import connectDB from "../db/db.js";
+
+const checkDBConnection = async () => {
+    if (mongoose.connection.readyState !== 1) {
+		await connectDB();
+	}
+}
+
+export default checkDBConnection;
